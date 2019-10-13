0bc7ecedd8d4be158961eb5cf93ffbeb3740fdf4
SanaJahan <sanadgrt@gmail.com>
2019-10-12 21:15:31 -0700

    Added the github link and removed contact navbar

--- my-work/src/components/HomePage.js
+++ my-work/src/components/HomePage.js
@@ -12,35 +12,28 @@ class HomePage extends Component {
                         <li><a href="#home">Home</a></li>
                         <li><a href="#about">About</a></li>
                         <li><a href="#projects">Projects</a></li>
-                        <li><a href="#contact">Contact</a></li>
                     </ul>
                 </nav>
                 <section id="home">
                     <img src={require('../images/sana.jpg')} alt={"Could not display"}/>
                     <h1>Sana Jahan</h1>
-                </section>
-                <section id="about">
-                    <h1>About</h1>
-                    <p>A full stack engineer by experience, a polygot(in machine and human languages) & a Master's student of
-                        Computer Science in Northeastern University.</p>
-                </section>
-                <section id="projects">
-                    <h1>Projects</h1>
-                    <ul>
-                        <li><a href={'https://www.github.com/SanaJahan'}> Project1</a></li>
-                        <li><a href={'https://www.github.com/SanaJahan'}> Project2</a></li>
-                        <li><a href={'https://www.github.com/SanaJahan'}> Project3</a></li>
-                    </ul>
-                </section>
-                <section id="contact">
-                    <h1>Contact</h1>
                     <p>
                         <div className={"iconPosition"}>
                             <a href={'https://linkedin.com/in/sanajahan'}><Icon icon={"linkedin2"}/></a>
                             <a href={'mailto:jahan.sana18@gmail.com'}><Icon icon={"google"}/></a>
-                            <a href="#about"><Icon icon={"facebook"}/></a>
                         </div>
                     </p>
+                </section>
+                <section id="about">
+                    <h1>About</h1>
+                    <p>A full stack engineer by experience, a polyglot (in machine and human languages).
+                    <p>Currently pursuing Master of Science in Computer Science from Northeastern University.</p></p>
+                </section>
+                <section id="projects">
+                    <ul>
+                        <li><a href={'https://www.github.com/SanaJahan'}> <img src={require('../images/github-logo.png')} /></a></li>
+                    </ul>
+                    <h1><a href={'https://www.github.com/SanaJahan'}>Projects</a></h1>
                 </section>
             </div>
         );
